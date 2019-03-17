f5a129a6feffc46cc878fa8e96dec09e16d9a719
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-03-17 12:00:01 +0100

    Add more information about the AST

--- src/main/kotlin/com/gumichan01/fkalculus/ast/Ast.kt
+++ src/main/kotlin/com/gumichan01/fkalculus/ast/Ast.kt
@@ -3,7 +3,7 @@ package com.gumichan01.fkalculus.ast
 // Abstract Syntax Tree (AST) of commands and expressions
 
 sealed class FKalculusAST
-object Help: FKalculusAST()
+object Help : FKalculusAST()
 sealed class Instruction : FKalculusAST()
 data class Eval(val expr: Expression) : Instruction()
 data class Simpl(val expr: Expression) : Instruction()
@@ -20,19 +20,27 @@ data class Var(val variable: String) : Expression()
 data class Binop(val operator: Operator, val expr1: Expression, val expr2: Expression) : Expression()
 data class Sqrt(val expr: Expression) : Expression()
 data class Expo(val expr: Expression) : Expression()
-data class Ln(val expr: Expression) : Expression()     /* Natural Logarithm */
+/* Natural Logarithm */
+data class Ln(val expr: Expression) : Expression()
+
+/* ∀x > 0, y ∈ ℝ, log₁₀(x) = y ↔ 10^ʸ = x; ISO 80000-2 notation: lg */
 data class Log10(val expr: Expression) : Expression()
+
+/* ∀x > 0, y ∈ ℝ, log₂(x) = y ↔ 2^ʸ = x; ISO 80000-2 notation: lb */
 data class Log2(val expr: Expression) : Expression()
+
 data class Sin(val expr: Expression) : Expression()
 data class Cos(val expr: Expression) : Expression()
 data class Tan(val expr: Expression) : Expression()
+/* Every A{cos/sine/tan} are inverse functions of cos/sin/tan */
 data class Asin(val expr: Expression) : Expression()
+
 data class Acos(val expr: Expression) : Expression()
 data class Atan(val expr: Expression) : Expression()
-data class Sec(val expr: Expression) : Expression()
-data class Cosec(val expr: Expression) : Expression()
-data class Cotan(val expr: Expression) : Expression()
-data class Asec(val expr: Expression) : Expression()
+data class Sec(val expr: Expression) : Expression()     /* Reciprocal function of Cosine */
+data class Cosec(val expr: Expression) : Expression()   /* Reciprocal function of Sine */
+data class Cotan(val expr: Expression) : Expression()   /* Reciprocal function of Tan */
+data class Asec(val expr: Expression) : Expression()    /* Every A{sec/cosec/cottan} are inverse functions of sec/cosec/cotan */
 data class Acosec(val expr: Expression) : Expression()
 data class Acotan(val expr: Expression) : Expression()
 
