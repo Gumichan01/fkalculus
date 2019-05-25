48797e33554c7532dcc0c6dff28ae8587c802a69
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-05-25 18:02:17 +0200

    Evaluate arcsin — inverse

--- src/test/kotlin/com/gumichan01/fkalculus/eval/TestEvaluator.kt
+++ src/test/kotlin/com/gumichan01/fkalculus/eval/TestEvaluator.kt
@@ -461,6 +461,17 @@ class TestEvaluator {
                 && (result.t as IdentifierValue).value == Asin(Binop(Plus, Var("x"), Const(1.0))))
     }
 
+    @test
+    fun `test arcsin is inverse of sine`() {
+        val ast = Asin(Sin(Const(90.0)))
+        val result: Option<ResultValue> = Evaluator().eval(ast)
+
+        assertTrue(result is Some)
+        assertTrue(result is Some && result.t is IdentifierValue)
+        assertTrue(checkConst(result))
+        assertTrue(result is Some && checkConst(result) && (result.t as IdentifierValue).value == Const(Math.asin(Math.sin(90.0))))
+    }
+
     private fun checkConst(result: Option<ResultValue>): Boolean {
         return result is Some && result.t is IdentifierValue && (result.t as IdentifierValue).value is Const
     }
