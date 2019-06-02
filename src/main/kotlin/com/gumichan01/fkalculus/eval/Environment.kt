3e0aa782f39a5379294007f9ef5fd6ec93567805
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-06-02 15:47:48 +0200

    Update the environment - base

--- src/main/kotlin/com/gumichan01/fkalculus/eval/Environment.kt
+++ src/main/kotlin/com/gumichan01/fkalculus/eval/Environment.kt
@@ -1,8 +1,25 @@
 package com.gumichan01.fkalculus.eval
 
+import com.gumichan01.fkalculus.ast.Const
+import com.gumichan01.fkalculus.ast.Expression
+
 class Environment {
 
+    var id: String? = null
+    var expr: Expression? = null
+
     override fun equals(other: Any?): Boolean {
-        return other != null
+        return when (other) {
+            is Environment -> id == other.id && expr == other.expr
+            else -> false
+        }
+    }
+
+    fun update(identifier: String, expression: Const): Environment {
+        val newEnv = Environment()
+        newEnv.id = identifier
+        newEnv.expr = expression
+
+        return newEnv
     }
 }
--- src/test/kotlin/com/gumichan01/fkalculus/eval/TestEnvironment.kt
+++ src/test/kotlin/com/gumichan01/fkalculus/eval/TestEnvironment.kt
@@ -1,5 +1,6 @@
 package com.gumichan01.fkalculus.eval
 
+import com.gumichan01.fkalculus.ast.Const
 import org.junit.jupiter.api.Test
 
 class TestEnvironment {
@@ -19,4 +20,14 @@ class TestEnvironment {
         assert(env1 == env2)
     }
 
+    @Test
+    fun `test update environment and check new envrironment`() {
+        val environment = Environment()
+        val identifier = "v1"
+        val expression = Const(42.0)
+
+        val newEnvironment : Environment = environment.update(identifier, expression)
+
+        assert(newEnvironment != environment)
+    }
 }
