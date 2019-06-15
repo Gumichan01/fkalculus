f887a40172e5e3105410d17a11768572ed6d75bf
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-06-15 22:49:26 +0200

    Fix packaging on Windows
    Pulling the repository of Launch4J may fail.
    When it is happening, the program relaunches maven in loop ad vitam aeternam.
    
    This commit fix that by chaining shell commands.

--- package-windows.sh
+++ package-windows.sh
@@ -9,12 +9,12 @@
 # This script is under CeCILL v2.1 license.
 #
 
-git clone https://github.com/Gumichan01/launch4j.git
-cd launch4j
-git checkout Release_launch4j-3_12
-ant switch-to-maven
-mvn clean package
-cp bin/bin-win32/*.exe bin/
-cd ..
-java -jar launch4j/launch4j-3.12.jar fkalculus.xml
+git clone https://github.com/Gumichan01/launch4j.git && \
+cd launch4j && \
+git checkout Release_launch4j-3_12 && \
+ant switch-to-maven && \
+mvn clean package && \
+cp bin/bin-win32/*.exe bin/ && \
+cd .. && \
+java -jar launch4j/launch4j-3.12.jar fkalculus.xml && \
 rm -rf launch4j/
